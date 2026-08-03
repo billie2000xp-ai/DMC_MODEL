92d95dfd0d986f0cf6f481bdfff2a6bbd27d141e
billie2000xp-ai <billie2000xp@gmail.com>
2026-08-03 10:46:39 +0800

    Update BankState.cpp

--- BankState.cpp
+++ BankState.cpp
@@ -1,10 +1,24 @@
+/* $$$!!Warning: Huawei key information asset. No spread without permission.$$$ */
+/* CODEMARK:RKeR1B8WMAfemkt1tTDGp4eOEddgxKn4NOPmdw0w+6Q3n1pxgDEX+kGBiRV20e1NKuLwOh60qWwx
+7DOUvTqsDpJdC/G6ahMCQuRlwWqc+IGKquH6vaaGAGe1zSmcLn5FMd2VBk0upEP5xKZPTVuBjKnw
+SvZMzBtMrQ+w1lxbG5+EFWux51V2bvtZUTAAA+en/pM7ZB5Cy3u0JTs1VqxXwmxUmALsnN13rsZ6
+elLFMxmcQvbA8P75QLAewnc3IkgyJMJQndhtFRm3uyY7ah2oFC9RtE/A70ZLkv78UbVpoXbRnfrm
+RTADgWNHjhYSSnfK# */
+/* $$$!!Warning: Deleting or modifying the preceding information is prohibited.$$$ */
+/*
+* Copyright @ Huawei Technologies Co., Ltd. 2019-2029. All rights reserved.
+* Description: BankState.cpp
+* Author     : l00434636
+* Create     : 2020-10-27
+*/
+
 #include "BankState.h"
 
 using namespace std;
 using namespace DRAMSim;
 
 //All banks start precharged
-BankState::BankState(ostream &DDRSim_log_):
+BankState:: BankState(ostream &DDRSim_log_): 
     DDRSim_log(DDRSim_log_),
     currentBankState(Idle),
     openRowAddress(0xFFFFFFFF),
@@ -53,10 +67,10 @@ void BankState::print() {
 
     PRINT("    OpenRowAddress  : " << openRowAddress);
     PRINT("    nextRead        : " << nextRead);
-    PRINT("    nextReadAp      : " << nextReadAp);
+    PRINT("   nextReadAp      : " << nextReadAp);
     PRINT("    nextWrite       : " << nextWrite);
     PRINT("    nextWriteAp     : " << nextWriteAp);
-    PRINT("    nextWriteRmw    : " << nextWriteRmw);
+    PRINT("   nextWriteRmw    : " << nextWriteRmw);
     PRINT("    nextWriteApRmw  : " << nextWriteApRmw);
     PRINT("    nextActivate1   : " << nextActivate1);
     PRINT("    nextActivate2   : " << nextActivate2);
